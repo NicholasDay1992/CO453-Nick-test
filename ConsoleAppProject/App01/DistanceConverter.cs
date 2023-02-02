a02f1fb3d641b52f9946d46028647cf7d6b63546
Nicholas Day <71455485+NicholasDay1992@users.noreply.github.com>
2023-02-02 17:27:58 +0000

    setup and start on app01

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -10,5 +10,43 @@ namespace ConsoleAppProject.App01
     /// </author>
     public class DistanceConverter
     {
+        public void Run()
+        {
+            ConvertFeetToMiles();
+            ConvertMilesToFeet();
+        }
+
+        public void ConvertMilesToFeet()
+        {
+            //step 1 - input the miles
+            Console.WriteLine("Enter the number of miles: ");
+            string input = Console.ReadLine();
+            int miles = Convert.ToInt32(input);
+
+            //step 2 - convert miles to feet
+
+            int feet = miles * 5280;
+
+            // step 3 - output to screen 
+
+            Console.WriteLine(miles + " miles is " + feet + " feet.");
+        }
+
+        public void ConvertFeetToMiles()
+        {
+            //step 1 - input the feet
+            Console.WriteLine("Enter the number of feet: ");
+            string input = Console.ReadLine();
+            double feet = Convert.ToDouble(input);
+
+            //step 2 - convert feet to miles
+
+            double miles = feet / 5280;
+
+            // step 3 - output to screen 
+
+            Console.WriteLine(feet + " feet is " + miles.ToString("0.00") + " miles.");
+
+        }
     }
 }
