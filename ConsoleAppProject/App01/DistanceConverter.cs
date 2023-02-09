85b0f3727087553702341a963c0e5b9482ff7d36
Nicholas Day <71455485+NicholasDay1992@users.noreply.github.com>
2023-02-09 17:33:41 +0000

    created separate methods for milestofeet and feettomiles

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -10,29 +10,54 @@ namespace ConsoleAppProject.App01
     /// </author>
     public class DistanceConverter
     {
+        const int MILES_TO_FEET = 5280;
+        string input;
+        int miles;
+        int feet;
+
         public DistanceConverter()
         {
-            
+
         }
         public void Run()
+        {
+            ConvertMilesToFeet();
+            ConvertFeetToMiles();
+        }
+
+        public void ConvertMilesToFeet()
         {
             //step 1 - input miles
 
             Console.WriteLine("Please enter the number of miles: ");
-            string input = Console.ReadLine();
-            int miles = Convert.ToInt32(input);
+            input = Console.ReadLine();
+            miles = Convert.ToInt32(input);
             Console.WriteLine("Miles entered is: " + miles);
 
             // step 2 - conversion process 
-            const int MILES_TO_FEET = 5280;
-            int feet = miles * MILES_TO_FEET;
+            
+            feet = miles* MILES_TO_FEET;
 
             // step 3 - output converted number of feet
 
             Console.WriteLine(miles + " miles is " + feet + " feet.");
-
-
         }
 
+        public void ConvertFeetToMiles()
+        {
+
+            //step 1 - input feet
+
+            Console.WriteLine("Please enter the number of feet: ");
+            input = Console.ReadLine();
+            feet = Convert.ToInt32(input);
+            Console.WriteLine("Feet entered is: " + feet);
+
+            // step 2 - conversion process 
+            miles = feet / MILES_TO_FEET;
+
+            // step 3 - output converted number of miles
+            Console.WriteLine(feet + " feet is " + miles + " miles.");
+        }
     }
 }
